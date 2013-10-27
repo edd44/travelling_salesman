a3949d7211f95d7e7c39a9fb3d390c4d796e44e9
edd44 <kaptur.michal@gmail.com>
2013-10-27 15:32:53 +0100

    Base program, opens and reads input file

---
+++ travelling_salesman/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace travelling_salesman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonShowDist = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(9, 17);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(122, 31);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Otwórz plik";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonShowDist
+            // 
+            this.buttonShowDist.Enabled = false;
+            this.buttonShowDist.Location = new System.Drawing.Point(11, 58);
+            this.buttonShowDist.Name = "buttonShowDist";
+            this.buttonShowDist.Size = new System.Drawing.Size(119, 27);
+            this.buttonShowDist.TabIndex = 1;
+            this.buttonShowDist.Text = "Pokaż odległości";
+            this.buttonShowDist.UseVisualStyleBackColor = true;
+            this.buttonShowDist.Click += new System.EventHandler(this.buttonShowDist_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonShowDist);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonShowDist;
+    }
+}
+
---
+++ travelling_salesman/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace travelling_salesman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //pointer to multidimensional array of distances between cities
+        int[,] distArray;
+        //cities count
+        int cityCount;
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "Text Files (.txt)|*.txt";
+            openDlg.InitialDirectory = Environment.CurrentDirectory;
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader strReader = new StreamReader(openDlg.FileName);
+                //read city count from first line
+                cityCount = Convert.ToInt32(strReader.ReadLine());
+                //init distance array based on # of cities
+                distArray = new int[cityCount,cityCount];
+
+                int x = 0;
+                while (!strReader.EndOfStream)
+                {
+                    string temp = strReader.ReadLine();
+                    string[] distances = temp.Split(' ');
+                    for (int y=0; y <cityCount; y++)
+                        distArray[x, y] = Convert.ToInt32(distances[y]);
+                    x++;
+                }
+                buttonShowDist.Enabled = true;
+                strReader.Close();
+            }
+        }
+
+        private void buttonShowDist_Click(object sender, EventArgs e)
+        {
+            string temp = "";
+            for (int x = 0; x < cityCount; x++)
+            {
+                for (int y = 0; y < cityCount; y++)
+                    temp += distArray[x, y].ToString() + " ";
+                temp += "\n";
+            }
+            MessageBox.Show(temp, "Macierz odległosci");
+        }
+    }
+}
